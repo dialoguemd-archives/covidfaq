347ef5b087eef19669f9c737b42b7b76395afedd
Jeremy Pinto <jerpint@gmail.com>
2020-03-20 19:09:46 -0400

    Add scraping script and dump results to json files

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -10,8 +10,8 @@ def remove_html_tags(data):
 
 
 def page_to_json(page_contents, fname):
-    with open(fname, 'w', encoding='utf-8') as fp:
-        json.dump(page_contents, fp, indent=4, ensure_ascii=False)
+    with open(fname, 'w') as fp:
+        json.dump(page_contents, fp)
 
 
 def get_page_contents(URL):
@@ -26,53 +26,22 @@ def get_page_contents(URL):
 
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
-    page_contents = {'document_URL': URL}
-
-    #Extract top page warnings, if any
-    warnings = soup.find_all(class_="frame frame-avisExclam frame-type-textmedia frame-layout-0")
-    if warnings:
-        #  warnings_text = [warning.get_text() for warning in warnings if warnings]
-        warnings_text = warnings[0].get_text()
-        warnings_html = warnings[0].prettify()
-
-        page_contents['warnings'] = {
-            'plaintext': warnings_text,
-            'html': warnings_html,
-            'URL': URL,
-        }
-
-    # Look for subjects and split them by header
     subjects = soup.find_all(class_='frame frame-default frame-type-textmedia frame-layout-0')
+
+    page_contents = {}
     for sub in subjects:
-
-
-        # Look for headers in each subject
-        raw_title = None
+        # Look for header
         if sub.find('h2'):
             raw_title = sub.find('h2').contents[0]
-        elif sub.find('h3'):
-            raw_title = sub.find('h3').contents[0]
-        elif sub.find('h4'):
-            raw_title = sub.find('h4').contents[0]
-
-        if raw_title:
             title = ' '.join(raw_title.split())
-            # Extract the text from a header block
             if sub.find(class_='ce-bodytext'):
-                sub_text = []
-                sub_html = sub.find(class_='ce-bodytext')
-                for text in sub_html.contents:
-                    sub_text.append(text.get_text())
+                raw_sub_text = sub.find(class_='ce-bodytext').contents
+                subject_text = []
+                for text in raw_sub_text:
+                    subject_text.append(text.get_text())
 
                 if title not in ['Avis', 'Notice']: # This is on every page
-                    #  sub_contents['plaintext'] = sub_text
-                    #  sub_contents['html'] = sub_html.prettify()
-                    #  sub_contents['URL'] = URL
-                    page_contents[title]={
-                        'plaintext': sub_text,
-                        'html': sub_html.prettify(),
-                        'URL': URL,
-                    }
+                    page_contents[title]=subject_text
 
     return page_contents
 
@@ -89,153 +58,19 @@ def get_english_page(URL, base_url='https://www.quebec.ca'):
     return en_URL
 
 
-def page_to_md(page_contents, page_url, response_filename, nlu_filename):
-    section_title = page_url.rsplit('/')[-2]
-
-    for idx, (question, answer) in enumerate(page_contents.items()):
-
-        intent = "web_content/" + str(idx) + section_title
-        with open(response_filename, "a") as f:
-            f.write("## " + str(idx) + section_title)
-            f.write("\n")
-            f.write("* " + intent)
-            f.write("\n")
-            f.write("  - " + " ".join(answer))
-            f.write("\n")
-            f.write("\n")
-
-        with open(nlu_filename, "a") as f:
-            f.write("## intent: " + intent)
-            f.write("\n")
-            for a in answer:
-                f.write("  - " + a)
-                f.write("\n")
-            f.write("\n")
-
-def get_faq_contents(faq_URL):
-
-    page = requests.get(faq_URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    faq_contents = {'document_URL': faq_URL}
-    all_panels = soup.find_all(class_='panel panel-default')
-
-    # a panel contains both the question and the answer
-    for panel in all_panels:
-        question = panel.find(class_='accordion-toggle')
-        answer = panel.find(class_='ce-bodytext')
-
-        if question and answer:
-            question_str = question.get_text()
-            answer_str = [answer.get_text()]
-            answer_html = [answer.prettify()]
-
-            faq_contents[question_str] = {
-                'URL': faq_URL,
-                'html': answer_html,
-                'plaintext': answer_str,
-            }
-
-
-    return faq_contents
-
-
-def get_mainpage_contents(mainpage_URL):
-
-    page = requests.get(mainpage_URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    page_contents = {'document_URL': mainpage_URL}
-
-    warnings = soup.find_all(class_="alert alert-warning")
-    if warnings:
-        #  warnings_text = [warning.get_text() for warning in warnings if warnings]
-        warnings_text = warnings[0].get_text()
-        warnings_html = warnings[0].prettify()
-
-        page_contents['warnings'] = {
-            'plaintext': warnings_text,
-            'html': warnings_html,
-            'URL': mainpage_URL,
-        }
-        #  page_contents['page alerts'] = [warning.get_text() for warning in warnings if warnings]
-
-    # Look for subjects and split them by subtopic
-    topics = soup.find_all(class_='frame frame-default frame-type-textmedia frame-layout-0')
-
-    for topic in topics:
-
-        # Look for headers in each subject
-        raw_title = None
-        if topic.find('h2'):
-            raw_title = topic.find('h2').contents[0]
-        if raw_title:
-            title = ' '.join(raw_title.split())
-            links_plaintext = [href.get_text() for href in topic.find_all('a')]
-            page_contents[title] = {
-                'plaintext': links_plaintext,
-                'html': topic.prettify(),
-                'URL': mainpage_URL,
-            }
-
-    return page_contents
-
-
 if __name__ == '__main__':
 
-    # faq page is different in structure so is parsed separately below.
     french_URLS = [
         'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/',
         'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/consignes-directives-contexte-covid-19/',
         'https://www.quebec.ca/sante/problemes-de-sante/a-z/informations-generales-sur-le-coronavirus/',
-        'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/stress-anxiete-et-deprime-associes-a-la-maladie-a-coronavirus-covid-19/',
-        'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/consignes-directives-contexte-covid-19/communautes-autochtones/',
-        'https://www.quebec.ca/famille-et-soutien-aux-personnes/aide-et-soutien/allocation-directe-cheque-emploi-service-une-modalite-de-dispensation-des-services-de-soutien-a-domicile/',
-        'https://www.quebec.ca/famille-et-soutien-aux-personnes/aide-financiere/programme-aide-temporaire-aux-travailleurs/',
-        'https://www.quebec.ca/education/aide-financiere-aux-etudes/remboursement/',
-        'https://www.quebec.ca/famille-et-soutien-aux-personnes/services-de-garde-durgence/',
-        'https://www.quebec.ca/gouv/covid19-fonction-publique/',
-
+        'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/stress-anxiete-et-deprime-associes-a-la-maladie-a-coronavirus-covid-19/'
     ]
 
-    # This is to have the files compatible with the Raza framework
-    # Note that content is being appended to the end of the .md files
-    # They need to be deleted if already present when generating them
-    # To generate the md files, switch convert_to_md to True
-    convert_to_md = False
-    responses_fr_fname = 'responses_fr.md'
-    nlu_fr_fname = 'nlu_fr.md'
-    responses_en_fname = 'responses_en.md'
-    nlu_en_fname = 'nlu_en.md'
-
-    # scrape the "regular pages" structures
     for count, fr_URL in enumerate(french_URLS):
-        print(count)
         en_URL = get_english_page(fr_URL)
         page_contents_fr = get_page_contents(fr_URL)
         page_contents_en = get_page_contents(en_URL)
 
         page_to_json(page_contents_fr, str(count) + '_fr.json')
         page_to_json(page_contents_en, str(count) + '_en.json')
-
-
-        if convert_to_md:
-            page_to_md(page_contents_fr, fr_URL, responses_fr_fname, nlu_fr_fname)
-            page_to_md(page_contents_en, en_URL, responses_en_fname, nlu_en_fname)
-
-
-    # scrape the faq
-    faq_URL_fr = 'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/reponses-questions-coronavirus-covid19/'
-    faq_URL_en = get_english_page(faq_URL_fr)
-    faq_contents_fr = get_faq_contents(faq_URL_fr)
-    faq_contents_en = get_faq_contents(faq_URL_en)
-    page_to_json(faq_contents_fr, 'faq_fr.json')
-    page_to_json(faq_contents_en, 'faq_en.json')
-
-    # scrape the main page
-    mainpage_URL_fr = 'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/'
-    mainpage_URL_en = get_english_page(mainpage_URL_fr)
-    mainpage_contents_fr = get_mainpage_contents(mainpage_URL_fr)
-    mainpage_contents_en = get_mainpage_contents(mainpage_URL_en)
-
-    page_to_json(mainpage_contents_fr, 'mainpage_fr.json')
-    page_to_json(mainpage_contents_en, 'mainpage_en.json')
