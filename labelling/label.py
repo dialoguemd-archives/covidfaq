1e785dbd576457c9b1561ba117b06d7559147705
Mirko Bronzi <m.bronzi@gmail.com>
2020-03-21 19:57:18 -0400

    added code to label user questions (to help manual annotation)

---
+++ labelling/label.py
@@ -0,0 +1,76 @@
+import torch
+from transformers import *
+import glob
+import os
+import json
+import pprint
+import csv
+import argparse
+
+def get_master_questions(folder, use_content):
+    master_questions = []
+    for f in glob.glob(os.path.join(folder, '*fr.json')):
+        print(f)
+        with open(f, 'r') as instream:
+            f_dict = json.load(instream)
+            for k, v in f_dict.items():
+                if use_content:
+                    if type(v) == list:  # fix for json format bug
+                        to_add = k + ' ' + ' '.join(v)
+                    else:
+                        to_add = k + ' ' + v
+                else:
+                    to_add = k
+                master_questions.append(to_add)
+    return master_questions
+
+
+def get_user_questions(file_path):
+    result = []
+    with open(file_path, 'r') as instream:
+        reader = csv.reader(instream)
+        for row in reader:
+            result.append(row[2])
+    return result
+
+
+def encode(sentence, tokenizer, model):
+    input_ids = torch.tensor([tokenizer.encode(sentence, add_special_tokens=True)])
+    with torch.no_grad():
+        last_hidden_states = model(input_ids)[0]
+        return last_hidden_states[0][0] # just the CLS.
+ 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mq-folder', help='folder containing master question json files')
+    parser.add_argument('--uq-csv', help='csv file containing user questions')
+    parser.add_argument('--use-content', help='use also the content of the master question'
+                        'paragraph for similarity', action='store_true')
+    args = parser.parse_args()
+
+    pretrained_weights = 'flaubert-large-cased'
+    tokenizer_class = FlaubertTokenizer
+    model_class = FlaubertModel
+    tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+    model = model_class.from_pretrained(pretrained_weights)
+
+    user_qs = get_user_questions(args.uq_csv)
+    encoded_user_qs = [encode(x, tokenizer, model) for x in user_qs]
+
+    master_qs = get_master_questions(args.mq_folder, args.use_content)
+    encoded_master_qs = [encode(x, tokenizer, model) for x in master_qs]
+
+    for i, user_q in enumerate(user_qs):
+        print('uq: {}'.format(user_q))
+        scores = []
+        for j, master_q in enumerate(master_qs):
+            dot = torch.dot(encoded_user_qs[i], encoded_master_qs[j])
+            scores.append((dot, master_q))
+        for dot, master_q in reversed(sorted(scores)):
+            print('\tmq: {} => {}'.format(master_q, dot))
+
+
+if __name__ == '__main__':
+        main()
+
---
+++ labelling/run_on_cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --partition=unkillable
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:1
+#SBATCH --mem=10G
+#SBATCH --time=0:20:00
+#SBATCH --output=out.log
+#SBATCH --error=err.log
+##SBATCH --mail-type=all
+##SBATCH --mail-user=$YOUR_EMAIL
+
+module load python/3.7
+module load cuda/10.0/cudnn/7.6 # cuda/10.0
+
+export MLFLOW_TRACKING_URI='mlruns'
+export ORION_DB_ADDRESS='orion_db.pkl'
+export ORION_DB_TYPE='pickleddb'
+
+python label.py --mq-folder ../../experiments/covidfaq/scrape/ --uq-csv ../../experiments/covidfaq/data/website_questions_21032020_13h00.csv
