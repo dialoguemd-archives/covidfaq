cf04b4697b68180591e49c73ed714ecf80c6a072
lp-noel <25254389+lp-noel@users.noreply.github.com>
2020-03-23 10:23:17 -0400

    add requirements to project and run from root

--- search/search_index.py
+++ search/search_index.py
@@ -1,48 +1,53 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import json
+from copy import deepcopy
+
+import pandas as pd
+import spacy
 ## Before running the script, install and start elastic search server on localhost port 9200 (by default)
 from elasticsearch import Elasticsearch
-import pandas as pd
-from tqdm.auto import tqdm
-from copy import deepcopy
-import json
-from build_index import en_doc_index, en_sec_index, fr_doc_index, fr_sec_index
-import spacy
 from spacy_langdetect import LanguageDetector
-nlp = spacy.load('en')
-nlp.add_pipe(LanguageDetector(), name='language_detector', last=True)
+from tqdm.auto import tqdm
+
+from build_index import en_doc_index, en_sec_index, fr_doc_index, fr_sec_index
+
+nlp = spacy.load("en")
+nlp.add_pipe(LanguageDetector(), name="language_detector", last=True)
 
 ## Config variables
 ################################
 
-question_file = './query_result_2020-03-22T22_05_26.81834Z.csv'
+question_file = "search/query_result_2020-03-22T22_05_26.81834Z.csv"
 
 ################################
 
+
 def detect_language(text):
-    return nlp(text)._.language['language']
+    return nlp(text)._.language["language"]
+
 
 def search_document_index(es, index, query, topk):
-    res = es.search({'query': {
-        'match': {
-            "content": query
-            }
-        },
-        'size': topk
-    }, index=index)
+    res = es.search({"query": {"match": {"content": query}}, "size": topk}, index=index)
     return res
 
-def search_section_index(es, index, query, topk, title_boost = 1):
-    res = es.search({'query': {
-        'multi_match': {
-            "query": query,
-            "fields": ["section^{}".format(title_boost), "content"]
-            }
+
+def search_section_index(es, index, query, topk, title_boost=1):
+    res = es.search(
+        {
+            "query": {
+                "multi_match": {
+                    "query": query,
+                    "fields": ["section^{}".format(title_boost), "content"],
+                }
+            },
+            "size": topk,
         },
-        'size': topk
-    }, index=index)
+        index=index,
+    )
     return res
+
 
 def query_question(es, q):
     lan = detect_language(q)
@@ -51,34 +56,37 @@ def query_question(es, q):
     topk_sec = 1
 
     def helper(docindex, secindex):
-        res_doc = search_document_index(es, docindex, q, topk_doc)['hits']['hits']
+        res_doc = search_document_index(es, docindex, q, topk_doc)["hits"]["hits"]
         if len(res_doc):
-            res_doc_txt = json.dumps(res_doc[0]['_source'])
+            res_doc_txt = json.dumps(res_doc[0]["_source"])
         else:
-            res_doc_txt = 'None'
-        res_sec = search_section_index(es, secindex, q, topk_sec)['hits']['hits']
+            res_doc_txt = "None"
+        res_sec = search_section_index(es, secindex, q, topk_sec)["hits"]["hits"]
         if len(res_sec):
-            res_sec_txt = json.dumps(res_sec[0]['_source'])
+            res_sec_txt = json.dumps(res_sec[0]["_source"])
         else:
-            res_sec_txt = 'None'
+            res_sec_txt = "None"
         return res_doc_txt, res_sec_txt
 
-    if lan == 'en':
+    if lan == "en":
         return helper(en_doc_index, en_sec_index)
-    elif lan == 'fr':
+    elif lan == "fr":
         return helper(fr_doc_index, fr_sec_index)
     else:
-        return ('None', 'None')
+        return ("None", "None")
+
 
 if __name__ == "__main__":
 
     ## Connect to the elastic cluster
 
-    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    es = Elasticsearch([{"host": "localhost", "port": 9200}])
     if not es.ping():
-        raise ValueError("Connection failed, please start server at localhost:9200 (default)")
+        raise ValueError(
+            "Connection failed, please start server at localhost:9200 (default)"
+        )
 
-    covid_questions = pd.read_csv(question_file)['question'].tolist()
+    covid_questions = pd.read_csv(question_file)["question"].tolist()
 
     all_results = []
 
@@ -88,19 +96,21 @@ if __name__ == "__main__":
 
         record = {}
 
-        print('$$ Question: ', q, '\n')
+        print("$$ Question: ", q, "\n")
 
         res_doc, res_sec = query_question(es, q)
 
-        print('=== Document search result: ', res_doc)
+        print("=== Document search result: ", res_doc)
         print()
-        print('=== Section search result: ', res_sec)
-        print('-' * 50)
+        print("=== Section search result: ", res_sec)
+        print("-" * 50)
 
-        record['question'] = q
-        record['document_result_json'] = res_doc
-        record['section_result_json'] = res_sec
+        record["question"] = q
+        record["document_result_json"] = res_doc
+        record["section_result_json"] = res_sec
 
         all_results.append(deepcopy(record))
 
-    pd.DataFrame(all_results, columns=['question', 'section_result_json', 'document_result_json']).to_csv('all_results.csv')
+    pd.DataFrame(
+        all_results, columns=["question", "section_result_json", "document_result_json"]
+    ).to_csv("search/all_results.csv")
