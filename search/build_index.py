a35e0ccc6aa613d01394538fe596e09c5f931f60
Louis-Philippe Noel <25254389+lp-noel@users.noreply.github.com>
2020-03-23 13:47:34 -0400

    feat: elastic search (#20)
    * Add scraping script and dump results to json files
    
    * Add indenting to json for readability
    
    * Fix encoding when printing
    
    * Add new link, add support for h3 headers too
    
    * Add new files
    
    * Add conversion to .md files for rasa
    
    * Add data in markdown format for rasa
    
    * Add missing whitespace
    
    * Update data from latest scrape
    
    * Add function to scrape faq website
    
    * Add faq questions and answers
    
    * Put answers to faq in a list for consistency
    
    * Update website scrape
    
    * Add top page warnings, if any, to the collected pages
    
    * Add scraping for the main page
    
    * Add main page scrape results
    
    * Change data structure for scraped sections
    
    * Adapt mainpage to new datastructure
    
    * Add document URL explicitly
    
    * add build and search index
    
    * save search results to csv for annotation
    
    * add search results for over 1700 questions in all_results.csv
    
    * add requirements to project and run from root
    
    * update dockerfile
    
    * Add scraping script and dump results to json files
    
    * Add new link, add support for h3 headers too
    
    * Add top page warnings, if any, to the collected pages
    
    * Change data structure for scraped sections
    
    * Adapt mainpage to new datastructure
    
    * Add document URL explicitly
    
    * add build and search index
    
    * save search results to csv for annotation
    
    * add search results for over 1700 questions in all_results.csv
    
    * add requirements to project and run from root
    
    * update dockerfile
    
    * structure logs
    
    * wippy wip
    
    * rebase
    
    * run scrape from dockerfile
    
    * poetry in docker
    
    * wip
    
    * Add scraping script and dump results to json files
    
    * Add new link, add support for h3 headers too
    
    * Add top page warnings, if any, to the collected pages
    
    * Change data structure for scraped sections
    
    * Adapt mainpage to new datastructure
    
    * Add document URL explicitly
    
    * add build and search index
    
    * save search results to csv for annotation
    
    * add search results for over 1700 questions in all_results.csv
    
    * add requirements to project and run from root
    
    * update dockerfile
    
    * Add scraping script and dump results to json files
    
    * Add indenting to json for readability
    
    * Fix encoding when printing
    
    * Add new link, add support for h3 headers too
    
    * Add new files
    
    * Add conversion to .md files for rasa
    
    * Add data in markdown format for rasa
    
    * Add missing whitespace
    
    * Update data from latest scrape
    
    * Add function to scrape faq website
    
    * Add faq questions and answers
    
    * Put answers to faq in a list for consistency
    
    * Update website scrape
    
    * Add top page warnings, if any, to the collected pages
    
    * Add scraping for the main page
    
    * Add main page scrape results
    
    * Change data structure for scraped sections
    
    * Adapt mainpage to new datastructure
    
    * Add document URL explicitly
    
    * add build and search index
    
    * save search results to csv for annotation
    
    * add search results for over 1700 questions in all_results.csv
    
    * add requirements to project and run from root
    
    * update dockerfile
    
    * structure logs
    
    * wippy wip
    
    * rebase
    
    * run scrape from dockerfile
    
    * poetry in docker
    
    * wip
    
    * connect
    
    * clean
    
    * reabse docker
    
    * clean
    
    Co-authored-by: Jeremy Pinto <jerpint@gmail.com>
    Co-authored-by: Maxime Belanger <maxime.belanger@dialogue.co>
    Co-authored-by: Hao <junhao@umich.edu>

---
+++ search/build_index.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import json
+from os import listdir
+from os.path import isfile, join
+
+import structlog
+
+## Before running the script, install and start elastic search server on localhost port 9200 (by default)
+from elasticsearch import Elasticsearch
+from tqdm.auto import tqdm
+
+log = structlog.get_logger(__name__)
+
+
+## Config variables
+################################
+
+scrape_path = "scrape/"
+assert scrape_path[-1] == "/"
+
+doc_url_key = "document_URL"
+
+en_doc_index = "en-covid-document-index"
+en_sec_index = "en-covid-section-index"
+fr_doc_index = "fr-covid-document-index"
+fr_sec_index = "fr-covid-section-index"
+
+################################
+
+
+def create_index(es):
+    es.indices.create(index=en_doc_index, ignore=400)
+    es.indices.create(index=en_sec_index, ignore=400)
+    es.indices.create(index=fr_doc_index, ignore=400)
+    es.indices.create(index=fr_sec_index, ignore=400)
+
+
+def delete_index(es):
+    es.indices.delete(index=en_doc_index, ignore=[400, 404])
+    es.indices.delete(index=en_sec_index, ignore=[400, 404])
+    es.indices.delete(index=fr_doc_index, ignore=[400, 404])
+    es.indices.delete(index=fr_sec_index, ignore=[400, 404])
+
+
+def fill_index(es, files, docindex, secindex):
+
+    c_d = 0
+    c_s = 0
+    for i in tqdm(files):
+        with open(i, "r", encoding="latin1") as f:
+            t = json.load(f)
+            t_ = {
+                j: {k: t[j][k] for k in t[j] if k in ["plaintext"]}
+                for j in t
+                if j != doc_url_key
+            }
+            doc = {"content": json.dumps(t_), "file_name": i, "url": t[doc_url_key]}
+            tmp = es.index(docindex, doc, id=i)
+            c_d += tmp["_shards"]["successful"]
+            c = 1
+            for sec in t:
+                if sec == doc_url_key:
+                    continue
+                rec = {
+                    "section": sec,
+                    "content": t[sec]["plaintext"],
+                    "file_name": i,
+                    "url": t[sec]["URL"],
+                }
+                tmp = es.index(secindex, rec, id=i + "_section_" + str(c))
+                c_s += tmp["_shards"]["successful"]
+                c += 1
+    log.info(
+        "Inserted/Updated documents and sections",
+        docindex=docindex,
+        secindex=secindex,
+        c_d=c_d,
+        c_s=c_s,
+    )
+
+    es.indices.refresh(index=docindex)
+    es.indices.refresh(index=secindex)
+
+    log.info(
+        "Total documents and sections",
+        docindex=docindex,
+        secindex=secindex,
+        c_d=es.cat.count(docindex, params={"format": "json"})[0]["count"],
+        c_s=es.cat.count(secindex, params={"format": "json"})[0]["count"],
+    )
+
+
+if __name__ == "__main__":
+
+    es = Elasticsearch(
+        [{"host": "es-covidfaq.dev.dialoguecorp.com", "port": 443}],
+        use_ssl=True,
+        verify_certs=True,
+    )
+    if not es.ping():
+        raise ValueError(
+            "Connection failed, please start server at localhost:9200 (default)"
+        )
+
+    ## Register index
+    delete_index(es)
+    create_index(es)
+
+    ## Load files
+    jsonfiles = [
+        f
+        for f in listdir(scrape_path)
+        if isfile(join(scrape_path, f))
+        if ".json" in f and "faq" not in f and "mainpage" not in f
+    ]
+    enfiles = [scrape_path + f for f in jsonfiles if "_en.json" in f]
+    frfiles = [scrape_path + f for f in jsonfiles if "_fr.json" in f]
+
+    fill_index(es, enfiles, en_doc_index, en_sec_index)
+    fill_index(es, frfiles, fr_doc_index, fr_sec_index)
