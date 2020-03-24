7c1a9baa01195f6ff04ed95b46b1eb2f40adea55
Louis-Philippe Noel <25254389+lp-noel@users.noreply.github.com>
2020-03-24 13:47:41 -0400

    Update main.py

--- covidfaq/main.py
+++ covidfaq/main.py
@@ -11,7 +11,6 @@ app.include_router(routers.answers.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
