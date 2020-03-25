431f6b3c3ff0af044fd6725ae8f34ddb4ded3f85
Louis-Philippe Noel <25254389+lp-noel@users.noreply.github.com>
2020-03-24 20:58:45 -0400

    feat: improve es (#41)

--- covidfaq/search/build_index.py
+++ covidfaq/search/build_index.py
@@ -46,28 +46,32 @@ def fill_index(es, files, docindex, secindex):
 
     c_d = 0
     c_s = 0
-    for i in tqdm(files):
-        with open(i, "r", encoding="utf-8") as f:
-            t = json.load(f)
-            t_ = {
-                j: {k: t[j][k] for k in t[j] if k in ["plaintext"]}
-                for j in t
+    for file_ in tqdm(files):
+        with open(file_, "r", encoding="utf-8") as f:
+            json_file = json.load(f)
+            content = {
+                j: {k: json_file[j][k] for k in json_file[j] if k in ["plaintext"]}
+                for j in json_file
                 if j != doc_url_key
             }
-            doc = {"content": json.dumps(t_), "file_name": i, "url": t[doc_url_key]}
-            tmp = es.index(docindex, doc, id=i)
+            doc = {
+                "content": json.dumps(content),
+                "file_name": file_,
+                "url": json_file[doc_url_key],
+            }
+            tmp = es.index(docindex, doc, id=file_)
             c_d += tmp["_shards"]["successful"]
             c = 1
-            for sec in t:
+            for sec in json_file:
                 if sec == doc_url_key:
                     continue
                 rec = {
                     "section": sec,
-                    "content": t[sec]["plaintext"],
-                    "file_name": i,
-                    "url": t[sec]["URL"],
+                    "content": json_file[sec]["plaintext"],
+                    "file_name": file_,
+                    "url": json_file[sec]["URL"],
                 }
-                tmp = es.index(secindex, rec, id=i + "_section_" + str(c))
+                tmp = es.index(secindex, rec, id=file_ + "_section_" + str(c))
                 c_s += tmp["_shards"]["successful"]
                 c += 1
     log.info(
@@ -108,10 +112,7 @@ if __name__ == "__main__":
 
     ## Load files
     jsonfiles = [
-        f
-        for f in listdir(scrape_path)
-        if isfile(join(scrape_path, f))
-        if ".json" in f and "faq" not in f and "mainpage" not in f
+        f for f in listdir(scrape_path) if isfile(join(scrape_path, f)) if ".json" in f
     ]
     enfiles = [scrape_path + f for f in jsonfiles if "_en.json" in f]
     frfiles = [scrape_path + f for f in jsonfiles if "_fr.json" in f]
