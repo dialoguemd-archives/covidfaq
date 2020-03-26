31db8f63ed7c495f2a0a0578eccab4dd5edbbc6b
lp-noel <25254389+lp-noel@users.noreply.github.com>
2020-03-26 13:31:51 -0400

    linting

--- covidfaq/rerank/deploy.py
+++ covidfaq/rerank/deploy.py
@@ -1,17 +1,11 @@
-import json
 import torch
-from torch import nn
 from train_retriever import BertEncoder, Retriver, RetriverTrainer
-from transformers import (
-    BertModel,
-    BertTokenizer,
-)
+from transformers import BertModel, BertTokenizer
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     ## Berts
-    model_str = 'bert-base-uncased'
+    model_str = "bert-base-uncased"
     tokenizer = BertTokenizer.from_pretrained(model_str)
     bert_question = BertModel.from_pretrained(model_str)
     bert_paragraph = BertModel.from_pretrained(model_str)
@@ -23,11 +17,11 @@ if __name__ == '__main__':
     ret = Retriver(encoder_question, encoder_paragarph, tokenizer)
 
     ret_trainee = RetriverTrainer(ret)
-    tmp = torch.load('__temp_weight_ddp_end.ckpt')
+    tmp = torch.load("__temp_weight_ddp_end.ckpt")
 
-    s1 = 'dialogue rules!'
-    s2 = 'covid sucks!'
-    s3 = 'We <3 chatbots'
-    q = 'When will we be able to go for team beers?'
-    ret_trainee.load_state_dict(tmp['state_dict'])
+    s1 = "dialogue rules!"
+    s2 = "covid sucks!"
+    s3 = "We <3 chatbots"
+    q = "When will we be able to go for team beers?"
+    ret_trainee.load_state_dict(tmp["state_dict"])
     predictions = ret_trainee.retriever.predict(q, [s1, s2, s3])
