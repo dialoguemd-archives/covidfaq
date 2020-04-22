5f29392e20567ae8fe4e3232a4011855bace096a
Louis-Philippe Noel <25254389+lp-noel@users.noreply.github.com>
2020-04-22 13:40:14 -0400

    Update covidfaq/config.py
    Co-Authored-By: Maxime Belanger <maxime.belanger@dialogue.co>

--- covidfaq/config.py
+++ covidfaq/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 class Config(BaseSettings):
     elastic_search_host: str = "faq-master"
     elastic_search_port: int = 9200
-    elastic_search_use_ssl: bool = True
+    elastic_search_use_ssl: bool = False
     elastic_search_verify_certs: bool = True
 
     class Config:
