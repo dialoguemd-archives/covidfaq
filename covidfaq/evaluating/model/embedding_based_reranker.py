6708e4b716c087a17bde7cad0861409be1a6ca44
mirkobronzi <m.bronzi@gmail.com>
2020-04-22 11:20:06 -0400

    fixed bug with cast

--- covidfaq/evaluating/model/embedding_based_reranker.py
+++ covidfaq/evaluating/model/embedding_based_reranker.py
@@ -2,8 +2,10 @@ import torch
 import tqdm
 import yaml
 from bert_reranker.models.load_model import load_model
+from bert_reranker.models.retriever_trainer import RetrieverTrainer
 from transformers import AutoTokenizer
 from yaml import load
+import pytorch_lightning as pl
 
 from covidfaq.evaluating.model.model_evaluation_interface import ModelEvaluationInterface
 
@@ -14,11 +16,18 @@ class EmbeddingBasedReRanker(ModelEvaluationInterface):
         with open(config, 'r') as stream:
             hyper_params = load(stream, Loader=yaml.FullLoader)
 
-        saved_model = hyper_params['saved_model']
+        ckpt_to_resume = hyper_params['ckpt_to_resume']
         tokenizer_name = hyper_params['tokenizer_name']
         tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
-        self.model = load_model(hyper_params, tokenizer, False)
-        self.model.load_state_dict(torch.load(saved_model))
+        model = load_model(hyper_params, tokenizer, False)
+
+        ret_trainee = RetrieverTrainer(model, None, None, None, None, None)
+
+        model_ckpt = torch.load(
+            ckpt_to_resume, map_location=torch.device("cpu")
+        )
+        ret_trainee.load_state_dict(model_ckpt["state_dict"])
+        self.model = ret_trainee.retriever
 
         self.p_embs = None  # to be set with collect_answers
         self.indices = None  # to be set with collect_answers
@@ -29,7 +38,7 @@ class EmbeddingBasedReRanker(ModelEvaluationInterface):
         for index, answer in tqdm.tqdm(answers.items()):
             cached = self.model.embed_paragraph(answer)
             cached_answers.append(cached.squeeze(0))
-            indices.append(index)
+            indices.append(int(index))
         self.p_embs = torch.stack(cached_answers)
         self.indices = indices
 
