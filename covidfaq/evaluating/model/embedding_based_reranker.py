2823c7d46183cfb8c4a1f2fd985a29fa193786c3
Mirko Bronzi <m.bronzi@gmail.com>
2020-04-24 14:54:14 -0400

    feat: add end-to-end evaluation (#68)
    * started adding evaluator
    
    * first working version
    
    * now working with embedding reranker
    
    * added cheating model
    
    * added progressbar
    
    * small refactoring
    
    * normalizing scores
    
    * fixed bug with cast
    
    * uploading the correct json file / fixed embedding model code
    
    * changed data format / added comments
    
    * flake8 fixes
    
    * updated data format for evaluator
    
    * fixed doc
    
    * added doc
    
    * fixed reported accuracy/latency format
    
    * run black
    
    * add git dep
    
    * fixed path in readme
    
    * fixed poetry command
    
    * try something
    
    * fix dep
    
    * fix dep and readme
    
    Co-authored-by: Jeremy Pinto <jerpint@gmail.com>
    Co-authored-by: lp-noel <25254389+lp-noel@users.noreply.github.com>

---
+++ covidfaq/evaluating/model/embedding_based_reranker.py
@@ -0,0 +1,57 @@
+import torch
+import tqdm
+import yaml
+from bert_reranker.models.load_model import load_model
+from bert_reranker.models.retriever_trainer import RetrieverTrainer
+from transformers import AutoTokenizer
+from yaml import load
+
+from covidfaq.evaluating.model.model_evaluation_interface import (
+    ModelEvaluationInterface,
+)
+
+
+class EmbeddingBasedReRanker(ModelEvaluationInterface):
+    """
+    Model based on the BERT embedding approach.
+    It will compute the embeddings for the various answers (and cache them).
+    When a question is provided, it only needs to compute the embedding for that question.
+    """
+
+    def __init__(self, config):
+        with open(config, "r") as stream:
+            hyper_params = load(stream, Loader=yaml.FullLoader)
+
+        ckpt_to_resume = hyper_params["ckpt_to_resume"]
+        tokenizer_name = hyper_params["tokenizer_name"]
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+        model = load_model(hyper_params, tokenizer, False)
+
+        ret_trainee = RetrieverTrainer(model, None, None, None, None, None)
+
+        model_ckpt = torch.load(ckpt_to_resume, map_location=torch.device("cpu"))
+        ret_trainee.load_state_dict(model_ckpt["state_dict"])
+        self.model = ret_trainee.retriever
+
+        self.p_embs = None  # to be set with collect_answers
+        self.indices = None  # to be set with collect_answers
+
+    def collect_answers(self, answers):
+        cached_answers = []
+        indices = []
+        for index, answer in tqdm.tqdm(answers):
+            cached = self.model.embed_paragraph(answer)
+            cached_answers.append(cached.squeeze(0))
+            indices.append(index)
+        self.p_embs = torch.stack(cached_answers)
+        self.indices = indices
+
+    def answer_question(self, question):
+        q_emb = self.model.embed_question(question)
+
+        relevance_scores = torch.matmul(q_emb, self.p_embs.squeeze(0).T).squeeze(0)
+
+        rerank_index = torch.argsort(-relevance_scores)
+        index_of_highest = rerank_index[0].detach().cpu()
+
+        return self.indices[index_of_highest]
---
+++ covidfaq/evaluating/model/fake_reranker.py
@@ -0,0 +1,16 @@
+from covidfaq.evaluating.model.model_evaluation_interface import (
+    ModelEvaluationInterface,
+)
+
+
+class FakeReRanker(ModelEvaluationInterface):
+    """
+    Returns always the same answer.
+    (use for debug)
+    """
+
+    def collect_answers(self, answers):
+        pass
+
+    def answer_question(self, question):
+        return 1
