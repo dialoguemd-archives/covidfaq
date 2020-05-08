b8cc3f79e55e69ca54aa20aae1e8797ed9561df3
Jeremy Pinto <jerpint@gmail.com>
2020-05-08 13:02:46 -0400

    feat: scrape ontario websites (#78)
    * Fix merge conflict with master
    
    * Add config file for ontario sites
    
    * Modify some fields for refactored code
    
    * Blackify
    
    * Remove unused import
    
    * Fix isort
    
    * add chromium driver to dockerfile
    
    * Fix dockerfile
    
    * add selenium
    
    * Add chromium browser to install
    
    * install chrome manually in dockerfile
    
    * fix typo
    
    * Remove slash
    
    * install wget
    
    * Another attempt to install chromium
    
    * install gnugpg
    
    * try gnugpg2
    
    * fix typo
    
    * add curl
    
    * Fix isort again
    
    * Add alberta sites for scraping
    
    * make the scraper more general
    
    * Remove unused code
    
    * Blackify
    
    * Add website to quebec scrape
    
    * Add chromedriver dependencies
    
    * Fix selenium so it runs headless
    
    * Fix dockerfile
    
    * Add more sites to scrape from quebec.ca
    
    * Add bc for scraping
    
    * add navigable strings to scrape
    
    Co-authored-by: lp-noel <25254389+lp-noel@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,26 @@ FROM python:3.7.6-slim-buster as base
 FROM base as builder
 
 RUN apt-get update \
-    && apt-get install -y --no-install-recommends gcc g++
+    && apt-get install -y --no-install-recommends gcc g++ wget gnupg2 curl
+
+# chromedriver dependencies
+RUN apt-get install -y libglib2.0 \
+    libnss3 \
+    libgconf-2-4 \
+    libfontconfig1
+
+# install google chrome
+RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+RUN dpkg -i google-chrome-stable_current_amd64.deb; exit 0
+RUN apt-get -fy install
+
+# install chromedriver
+RUN apt-get install -yqq unzip
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+
+# set display port to avoid crash
+ENV DISPLAY=:99
 
 # permit installing private packages
 
--- covidfaq/scrape/scrape.py
+++ covidfaq/scrape/scrape.py
@@ -3,6 +3,7 @@ import json
 import os
 import re
 import sys
+import time
 from collections import defaultdict
 from datetime import datetime
 from urllib.parse import urljoin
@@ -11,7 +12,8 @@ import bs4
 import requests
 import structlog
 import yaml
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
+from selenium import webdriver
 from yaml import load
 
 log = structlog.get_logger(__name__)
@@ -91,8 +93,13 @@ def rule_nesting(soup, info, url, rule):
                         nested_titles[n] = ""
                 prev_nest_level = nest_level
 
-        if exclude["title"] and re.search(exclude["title"], raw_title):
-            continue
+        if exclude["titles"]:
+            skip_entry = False
+            for title in exclude["titles"]:
+                if re.search(title, raw_title):
+                    skip_entry = True
+            if skip_entry:
+                continue
 
         if not body:
             continue
@@ -117,6 +124,57 @@ def rule_nesting(soup, info, url, rule):
         }
         entry.update(info)
         entry.update(rule["info"])
+        results.append(entry)
+
+    return results
+
+
+def rule_headers(soup, info, url, rule):
+
+    results = []
+    exclude = rule["exclude"]
+
+    if rule["header_type"] == "goa":
+        headers = soup.select(rule["headers"][0])
+    elif rule["header_type"] == "markdown":
+        headers = soup.find_all(rule["headers"])
+
+    for header in headers:
+        nextNode = header
+        header_str = str(nextNode.text)
+        if exclude["titles"]:
+            skip_header = False
+            for title in exclude["titles"]:
+                if re.search(title, header_str):
+                    skip_header = True
+            if skip_header:
+                continue
+        entry = {
+            "nested_title": [],  # TODO: implement
+            "title": header_str,
+            "plaintext": [],
+            "url": url,
+            "html": "",
+            "time": info["time"],
+            "language": info["language"],
+        }
+        entry.update(info)
+        entry.update(rule["info"])
+        while True:
+            nextNode = nextNode.nextSibling
+            if nextNode is None:
+                break
+            elif nextNode.name in rule["headers"]:
+                break
+            elif isinstance(nextNode, NavigableString):
+                entry["plaintext"].append(str(nextNode))
+                entry["html"] += str(nextNode)
+                continue
+            if nextNode.text:
+                if nextNode.text != "":  # add and isinstance(nextNode, Tag)?
+                    entry["plaintext"].append(str(nextNode.text))
+                    entry["html"] += str(nextNode)
+
         results.append(entry)
 
     return results
@@ -155,9 +213,29 @@ def rule_sibling(soup, info, url, rule):
     return results
 
 
-def extract_sections(url, info, cfg, translated=False):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
+def get_soup(url, cfg, browser):
+
+    if cfg["scraper"] == "selenium":
+        wait_time = 4  # Kind of arbitrary, but if internet is slow page doesnt render on time because of js
+
+        browser.get(url)
+        # Make sure the page loads, might want to fix this for something more robust
+        time.sleep(wait_time)
+        html = browser.page_source
+        if html:
+            soup = BeautifulSoup(html, "lxml")
+        else:
+            log.info("scraping didn't work for ", url=url)
+            return None
+
+    elif cfg["scraper"] == "requests":
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+    return soup
+
+
+def extract_sections(url, info, cfg, browser, translated=False):
+    soup = get_soup(url, cfg, browser)
 
     if not translated and "translate" in cfg:
         new_url = soup.select_one(cfg["translate"])
@@ -165,7 +243,7 @@ def extract_sections(url, info, cfg, translated=False):
             return [], []
         new_url = urljoin(url, new_url["href"])
         info["url"] = new_url
-        return extract_sections(new_url, info, cfg, True)
+        return extract_sections(new_url, info, cfg, browser, True)
 
     log.info("scraping", urlkey=info["urlkey"], url=url)
 
@@ -185,6 +263,16 @@ def run(yaml_filename, outdir="covidfaq/scrape", formatting="old", site=None):
         sites = load(stream, Loader=yaml.FullLoader)
     now = str(datetime.now())
 
+    # Initialize broswer for selenium
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--remote-debugging-port=9222")
+    browser = webdriver.Chrome(options=options)
+
+    # Scrape sites
     results = []
     soups = []
     for sitename, sitecfg in sites.items():
@@ -199,11 +287,14 @@ def run(yaml_filename, outdir="covidfaq/scrape", formatting="old", site=None):
             urlinfo.setdefault("urlkey", str(i))
             urlinfo["urlkey"] = f"{sitename}-{urlinfo['urlkey']}"
             info = {**sitecfg["info"], **urlinfo, "time": now, "url": url}
-            result, soup = extract_sections(url, info, sitecfg)
+            result, soup = extract_sections(url, info, sitecfg, browser)
             if result:
                 results += result
                 soups.append(soup)
 
+    browser.quit()
+
+    # Save scraping results
     if formatting == "old":
         files = defaultdict(dict)
         # change key names
