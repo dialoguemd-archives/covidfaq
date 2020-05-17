580cdbacd96e06092454ed28fa8db174501afaf3
Xing Han <xhlperso@gmail.com>
2020-05-17 13:31:38 -0400

    Remove duplicate `build_isolator`

--- covidfaq/evaluating/model/lsa.py
+++ covidfaq/evaluating/model/lsa.py
@@ -7,13 +7,6 @@ from covidfaq.evaluating.model.model_evaluation_interface import (
     ModelEvaluationInterface,
 )
 
-def build_isolator(chars):
-    def isolate(text):
-        for c in chars:
-            text = text.replace(c, f" {c}")
-        return text
-    return isolate
-
 
 class LSA(ModelEvaluationInterface):
     def collect_answers(self, answers):
