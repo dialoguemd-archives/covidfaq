6fac248e255353e77b15f9cb1ff4b06163c5d25a
Maxime Belanger <maxime.belanger@dialogue.co>
2020-06-10 11:41:18 -0400

    restart the app after scrape:wq

---
+++ covidfaq/k8s.py
@@ -0,0 +1,25 @@
+import subprocess
+import structlog
+
+from typing import Any, Optional
+
+log = structlog.get_logger(__name__)
+
+
+class ShellError(Exception):
+    pass
+
+
+def rollout_restart():
+    log.info("restarting app")
+    shell("kubectl --namespace covidfaq rollout restart deployment app --wait")
+    log.info("restart app in progress")
+
+
+def shell(cmd: str, cwd: Optional[str] = None) -> Any:
+    try:
+        return subprocess.check_output(
+            cmd, cwd=cwd, shell=True, text=True, stderr=subprocess.STDOUT
+        )
+    except subprocess.CalledProcessError as e:
+        raise ShellError(e.output)
--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,4 +1,6 @@
 from covidfaq.scrape import scrape
+from covidfaq.k8s import rollout_restart
 
 if __name__ == "__main__":
     scrape.run("covidfaq/scrape/quebec-sites.yaml", "covidfaq/scrape/")
+    rollout_restart()
