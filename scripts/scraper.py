8dd5c113c8848af1e431465b69d42f49c7d9e83e
Jeremy Pinto <jerpint@gmail.com>
2020-06-18 22:48:18 -0400

    upload ood to s3

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,19 +1,21 @@
 import os
 
 import boto3
+import structlog
 
 from covidfaq.scrape import scrape
 from covidfaq.k8s import rollout_restart
 from covidfaq.evaluating.model.bert_plus_ood import BertPlusOOD
 
+log = structlog.get_logger()
+
 
 def upload_ood_to_s3():
-
     client = boto3.client("s3")
     BUCKET_NAME = os.environ.get("BUCKET_NAME")
     file_to_upload = ("covidfaq/bert_en_model/ood_model.pkl",)
-
     client.upload_file(file_to_upload, BUCKET_NAME, "ood_model.pkl")
+    log.info("OOD model uploaded to s3 bucket")
 
 
 def instantiate_OOD():
@@ -23,7 +25,8 @@ def instantiate_OOD():
 
 
 if __name__ == "__main__":
-    scrape.run("covidfaq/scrape/quebec-sites-small.yaml", "covidfaq/scrape/")
+    scrape.run("covidfaq/scrape/quebec-sites.yaml", "covidfaq/scrape/")
     # This will train the OOD detector
     BertPlusOOD()
+    upload_ood_to_s3()
     rollout_restart()
